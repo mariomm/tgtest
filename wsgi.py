f7e27babbadc7e2052ea3037011b967716fc3373
mariomm <764420+mariomm@users.noreply.github.com>
2018-03-19 14:09:05 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,16 @@
+from telethon import TelegramClient
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    
+    client = TelegramClient('session_name', api_id, api_hash)
+    client.start()
+    #print(client.get_me().stringify())
+    client.send_message('@bot', 'Diesel')
 
+    return "Hello World!"
+    
 if __name__ == "__main__":
-    application.run()
+application.run()
