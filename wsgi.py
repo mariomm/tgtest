4c109585460e949482780d51eb35c89625e97391
mariomm <764420+mariomm@users.noreply.github.com>
2018-03-20 09:53:39 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,17 @@
+import os
 from telethon import TelegramClient
 from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    
+    api_id = os.environ['apiidWebHookSecretKey']
+    api_hash = os.environ['apihashWebHookSecretKey']
+
     client = TelegramClient('session_name', api_id, api_hash)
     client.start()
     #print(client.get_me().stringify())
-    client.send_message('@bot', 'Diesel')
+    client.send_message('@' + os.environ['botnameWebHookSecretKey'], 'Diesel')
 
     return "Hello World!"
     
