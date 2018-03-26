5537360ad78093cd692b2ad0fe61cdd2f747ec29
mariomm <764420+mariomm@users.noreply.github.com>
2018-03-26 09:09:14 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,10 +8,12 @@ def hello():
     api_id = os.environ['apiidWebHookSecretKey']
     api_hash = os.environ['apihashWebHookSecretKey']
     botname = os.environ['botnameWebHookSecretKey']
+    d = parse_qs(environ['QUERY_STRING'])
+    age = d.get('age', [''])[0]
     
     client = TelegramClient('session_name', api_id, api_hash)
     client.start()   
-    client.send_message('@' + botname, 'Diesel')
+    client.send_message('@' + botname, age)
 
     return "Hello!"
     
