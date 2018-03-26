261510168052498221441ee396179ff50c6e4d3a
mariomm <764420+mariomm@users.noreply.github.com>
2018-03-26 10:05:04 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,6 @@
 import os
 from telethon import TelegramClient
 from flask import Flask
-from cgi import parse_qs
 application = Flask(__name__)
 
 @application.route("/")
@@ -9,12 +8,11 @@ def hello(environ, start_response):
     api_id = os.environ['apiidWebHookSecretKey']
     api_hash = os.environ['apihashWebHookSecretKey']
     botname = os.environ['botnameWebHookSecretKey']
-    d = parse_qs(environ['QUERY_STRING'])
-    age = d.get('age', [''])[0]
+   
     
     client = TelegramClient('session_name', api_id, api_hash)
     client.start()   
-    client.send_message('@' + botname, age)
+    client.send_message('@' + botname, 'Diesel')
 
     return "Hello!"
     
