221f37541a0a3c17194f52e9a8e466d01b4e9a04
mariomm <764420+mariomm@users.noreply.github.com>
2018-11-19 08:27:18 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 # http://url.com?type=Diesel
 
 import os
-from telethon import TelegramClient
+from telethon import TelegramClient, sync
 from flask import Flask, request
 application = Flask(__name__)
 
@@ -11,8 +11,8 @@ def hello():
     api_hash = os.environ['apihashWebHookSecretKey']
     botname = os.environ['botnameWebHookSecretKey']
     typ = request.args.get('type')
-    parama = request.args.get('parama')
-    paramb = request.args.get('paramb')
+    #parama = request.args.get('parama')
+    #paramb = request.args.get('paramb')
     client = TelegramClient('session_name', api_id, api_hash)
     client.start()   
     client.send_message('@' + botname, typ)
