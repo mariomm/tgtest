ea07bb2f193cb6ae0e85c19cec8bc11531aae1c3
mariomm <764420+mariomm@users.noreply.github.com>
2018-11-21 07:27:23 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@
 
 import os
 from telethon import TelegramClient, sync
+from telethon.sessions import StringSession
 from flask import Flask, request
 application = Flask(__name__)
 
@@ -10,12 +11,14 @@ def hello():
     api_id = os.environ['apiidWebHookSecretKey']
     api_hash = os.environ['apihashWebHookSecretKey']
     botname = os.environ['botnameWebHookSecretKey']
-    typ = request.args.get('type')   
+    typ = request.args.get('type')
     client = TelegramClient('session_name', api_id, api_hash)
-    client.start()   
+    client.start()
     client.send_message('@' + botname, typ)
-
+	#for message in client.get_message_history('@' + botname, limit=10):
+    #print(utils.get_display_name(message.sender), message.message)
+    client.disconnect()
     return "Hello!"
-    
+
 if __name__ == "__main__":
   application.run()
